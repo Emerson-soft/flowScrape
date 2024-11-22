40243a48d9c51ec05c0d4babddc57659f9421c64
Emerson <emerson_10_s@hotmail.com>
2024-11-22 19:49:39 -0300

    create sidebar mobile

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import { CoinsIcon, HomeIcon, Layers2Icon, ShieldCheckIcon } from 'lucide-react';
-import React from 'react'
+import { CoinsIcon, HomeIcon, Layers2Icon, MenuIcon, ShieldCheckIcon } from 'lucide-react';
+import React, { useState } from 'react'
 import Logo from './Logo';
 import Link from 'next/link';
-import { buttonVariants } from './ui/button';
+import { Button, buttonVariants } from './ui/button';
 import { usePathname } from 'next/navigation';
+import { Sheet, SheetContent, SheetTrigger } from './ui/sheet';
 
 const routes = [
   {
@@ -42,11 +43,11 @@ function DesktopSidebar() {
       <div className="p-2">TODO CREDITS</div>
       <div className="flex flex-col p-2">
         {routes.map((route) => (
-          <Link 
-            key={route.href} 
+          <Link
+            key={route.href}
             href={route.href}
             className={buttonVariants({
-              variant: 
+              variant:
                 activeRoute.href === route.href ? 'sidebarActiveItem' : 'sidebarItem',
             })}
           >
@@ -55,6 +56,47 @@ function DesktopSidebar() {
           </Link>
         ))}
       </div>
+    </div>
+  )
+}
+
+export function MobileSidebar() {
+  const [isOpen, setOpen] = useState(false);
+
+  const pathname = usePathname();
+  const activeRoute = routes.find((route) => route.href.length > 0 && pathname.includes(route.href)) || routes[0];
+
+  return (
+    <div className='block border-separate bg-background md:hidden'>
+      <nav className=''>
+        <Sheet open={isOpen} onOpenChange={setOpen}>
+          <SheetTrigger asChild>
+            <Button variant={"ghost"} size={"icon"}>
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+          <SheetContent className='w-[400px] sm:w-[540px] space-y-4' side={"left"}>
+            <Logo />
+
+            <div className="flex flex-col gap-1">              
+              {routes.map((route) => (
+                <Link
+                  key={route.href}
+                  href={route.href}
+                  className={buttonVariants({
+                    variant:
+                      activeRoute.href === route.href ? 'sidebarActiveItem' : 'sidebarItem',
+                  })}
+                  onClick={() => setOpen((prev) => !prev)}
+                >
+                  <route.icon size={20} />
+                  {route.label}
+                </Link>
+              ))}
+            </div>
+          </SheetContent>
+        </Sheet>
+      </nav>
     </div>
   )
 }
