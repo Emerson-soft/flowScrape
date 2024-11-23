9e71d642d365160f3524773551cd5f53abfa3296
Emerson <emerson_10_s@hotmail.com>
2024-11-23 09:32:00 -0300

    authentication implementation with clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { AppProvider } from "@/components/providers/AppProviders";
+import { ClerkProvider } from '@clerk/nextjs';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,12 +17,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <AppProvider>
-          {children}
-        </AppProvider>
-      </body>
-    </html>
+    <ClerkProvider 
+      afterSignOutUrl={"/sign-in"}
+      appearance={{
+        elements: {
+          formButtonPrimary: "bg-primary hove:bg-primary/90 text-sm !shadow-none"
+        }
+      }}
+    >
+      <html lang="en">
+        <body className={inter.className}>
+          <AppProvider>
+            {children}
+          </AppProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
